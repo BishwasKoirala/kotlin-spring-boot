0ff86df338b5e90df735b7dff7da805c92ceb367
Bishwas Koirala <wasubisu69@gmail.com>
2024-08-05 12:15:09 +0900

    next => add database support or spring boot project

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,28 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+
+class MessageController {
+	@GetMapping("/")
+	fun index() = listOf(
+		Message("1" , "Hello"),
+		Message("2" , "Namaste"),
+		Message("2" , "Bonkour"),
+		Message("4" , "Konnichiwa"),
+	)
+}
+
+data class  Message(val id : String? , val text : String)
